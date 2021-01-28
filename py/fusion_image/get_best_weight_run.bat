05473abab024e3303c39eec0a7dfd8970465ab6d
miwazawa <61324099+t.miwazawa@adacotech.co.jp>
2021-01-28 11:15:00 +0900

    rename dir

---
+++ py/fusion_image/get_best_weight_run.bat
@@ -0,0 +1,5 @@
+call C:\tools\Anaconda3\Scripts\activate.bat hlac-framework
+
+python .\get_best_weight.py get_best_weight_conf.yaml
+
+pause
--- script/fusion_image/get_best_weight_run.bat
+++
@@ -1,5 +0,0 @@
-call C:\tools\Anaconda3\Scripts\activate.bat hlac-framework
-
-python .\get_best_weight.py get_best_weight_conf.yaml
-
-pause
